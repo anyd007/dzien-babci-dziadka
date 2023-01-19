8a14fdceeeb4186abdd0534c53c9cd96e8abcc1e
Andrzej Gębala <agebala@neonet.pl>
2023-01-19 17:00:30 +0100

    added header and hero section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,7 +1,9 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".hero__wish-container ")
+const wishInstruction = document.querySelector(".hero__wish-container--info")
 
 const show = () =>{
-    container.classList.add("active")
+    container.classList.add("open")
+    wishInstruction.classList.add("open")
 }
 
 container.addEventListener("click", show)
