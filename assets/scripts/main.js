df252c09dc91ee32b82381f0b61df2d65f77ba9b
Andrzej Gębala <agebala@neonet.pl>
2023-01-20 13:03:46 +0100

    create gift section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,11 +1,34 @@
 const container = document.querySelector(".hero__wish-container ")
 const wishInstructionBtn = document.querySelector(".hero__wish-container--btn")
 const giftBow = document.querySelector(".hero__wish-container--img")
+const imgs = [...document.querySelectorAll(".imgs")]
+const btns = [...document.querySelectorAll(".btns")]
+const modalContainer = document.querySelector(".modal-container")
+const modalImg = document.querySelector(".modal-img")
+const closeModal = document.querySelector(".modal-icon")
 
-const show = () =>{
+btns.forEach((btn, btnIndex) =>{
+    btn.addEventListener("click", () =>{
+        imgs.forEach((img, imgIndex) =>{
+            if(btnIndex === imgIndex){
+                modalImg.src = img.src
+                modalContainer.classList.add("open")
+                document.body.classList.add("stopScroll")
+            }
+        })
+    })
+})
+
+const showGift = () =>{
     container.classList.add("open")
     wishInstructionBtn.classList.add("open")
     giftBow.classList.add("open")
 }
 
-container.addEventListener("click", show)
+const hideModal = () =>{
+    modalContainer.classList.remove("open")
+    document.body.classList.remove("stopScroll")
+}
+
+container.addEventListener("click", showGift)
+closeModal.addEventListener("click", hideModal)
